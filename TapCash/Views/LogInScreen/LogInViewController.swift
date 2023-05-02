3472f91a5e829212a0975584439a24a799a2904d
Mahmoud810 <57998877+Mahmoud810@users.noreply.github.com>
2023-05-03 01:55:42 +0300

    Stashed from TAP-2-Design-Login-Screen at 48d7903

---
+++ TapCash/Views/LogInScreen/LogInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  TapCash
+//
+//  Created by Mahmoud on 03/05/2023.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
