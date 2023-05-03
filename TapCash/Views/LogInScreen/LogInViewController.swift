fc371fe372219b77e6aa9c1e231e6a1669a206cf
Mahmoud810 <57998877+Mahmoud810@users.noreply.github.com>
2023-05-04 02:22:48 +0300

    desgin login screen

--- TapCash/Views/LogInScreen/LogInViewController.swift
+++ TapCash/Views/LogInScreen/LogInViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!{
+        didSet{
+            emailText.layer.cornerRadius = 20
+        }
+    }
+    @IBOutlet var passwordText: UITextField!{
+        didSet{
+            passwordText.layer.cornerRadius = 20
+        }
+    }
+    @IBOutlet weak var loginButton: UIButton!{
+        didSet{
+            loginButton.layer.cornerRadius = 20
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +31,8 @@ class LogInViewController: UIViewController {
     }
     
 
-   
+    @IBAction func loginAction(_ sender: UIButton) {
+    }
+    
 
 }
