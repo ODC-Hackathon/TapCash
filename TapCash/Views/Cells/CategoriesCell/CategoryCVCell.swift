8a11e46f0fa9ef82e6b2df6c5919f438db1a8a01
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-05-04 21:45:58 +0300

    TabBar Added

---
+++ TapCash/Views/Cells/CategoriesCell/CategoryCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCVCell.swift
+//  TapCash
+//
+//  Created by Adham Samer on 04/05/2023.
+//
+
+import UIKit
+
+class CategoryCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
