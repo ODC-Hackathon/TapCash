ee286234e8315c94a475378f203756db707e5d43
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-05-04 20:14:11 +0300

    Fix issues

--- TapCash/Views/AuthenticationScreens/LoginVC.swift
+++ TapCash/Views/AuthenticationScreens/LoginVC.swift
@@ -8,38 +8,35 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
-    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet var emailText: UITextField!
     @IBOutlet var passwordText: UITextField!
-    @IBOutlet weak var loginButton: UIButton!{
-        didSet{
+    @IBOutlet var loginButton: UIButton! {
+        didSet {
             loginButton.layer.cornerRadius = 20
         }
     }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
     override func viewDidAppear(_ animated: Bool) {
-        renderTextField(textFields: [emailText,passwordText])
+        renderTextField(textFields: [emailText, passwordText])
     }
 
     @IBAction func loginAction(_ sender: UIButton) {
-        
     }
-    
-
 }
 
-extension LoginVC{
-    func renderTextField(textFields: [UITextField]){
+extension LoginVC {
+    func renderTextField(textFields: [UITextField]) {
         for textField in textFields {
             textField.layer.masksToBounds = true
-            textField.layer.cornerRadius = 20
-            textField.layer.shadowRadius = 3
-            
+            textField.layer.cornerRadius = 10
+            textField.layer.borderColor = UIColor(named: "AccentColor")?.cgColor
+            textField.layer.borderWidth = 0.7
         }
-        
     }
 }
