16b2edecb98e6f7ccfdbeb4cdb6e9c13ed9f6fc3
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-05-08 13:51:58 +0300

    Added User Model

--- TapCash/Models/User.swift
+++ TapCash/Models/User.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+// MARK: - Users
+struct Users {
+    let data: [Datum]
+    let token: String
+}
+
+// MARK: - Datum
+struct Datum {
+    let userId: Int
+    let email: String
+    let user: User
+}
+
+// MARK: - User
+struct User {
+    let id: Int
+    let name, userName: String
+    let family: [Family]
+}
+
+// MARK: - Family
+struct Family {
+    let userName, name: String
+    let sponsorId: Int
+}
