e5ccfe8d73924e03e7162d5cda338f9ab446b130
Adham Samer <48602871+AdhamSamer3301@users.noreply.github.com>
2023-05-08 14:50:16 +0300

    ViewModel Added

--- TapCash/Models/NetworkServices/Protocols.swift
+++ TapCash/Models/NetworkServices/Protocols.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol GET_DATA{
+    static func fetch<T: Decodable>(url: String?, compiletionHandler: @escaping (T?) -> Void)
+}
+
+protocol POST_DATA {
+    static func postMethod(url: String, parameters: [String: Any],token:String?, completionHandler: @escaping ([String:Any]?)->Void)
+}
+
+protocol PUT_DATA {
+    static func putMethod(url: String, parameters: [String: Any],token:String?, handler: @escaping ([String: Any]?) -> Void)
+}
+
+protocol DELTE_DATA {
+    static func delete(url: String)
+}
