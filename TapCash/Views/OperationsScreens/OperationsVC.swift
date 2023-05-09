2f3d2c6fe0be773ac1725935766d3e30ee1e9006
Mahmoud810 <57998877+Mahmoud810@users.noreply.github.com>
2023-05-09 04:23:09 +0300

    implement login screen and add Operation implementation again

---
+++ TapCash/Views/OperationsScreens/OperationsVC.swift
@@ -0,0 +1,72 @@
+//
+//  OperationsVC.swift
+//  TapCash
+//
+//  Created by Mahmoud on 08/05/2023.
+//
+
+import UIKit
+import AudioToolbox
+class OperationsVC: UIViewController {
+    
+    @IBOutlet weak var amount: UILabel!
+    var totalAmount : Int = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DispatchQueue.main.async {
+            self.amount.textColor = .systemGray5
+            self.amount.text! += " \(self.totalAmount)"
+        }
+        
+        
+    }
+    
+    
+    @IBAction func addNumber(_ sender: UIButton) {
+        if sender.tag == 0 && totalAmount == 0{
+            
+        }
+        else if(totalAmount <= 80000)
+        {
+             self.amount.textColor = UIColor(named: "AccentColor")
+            totalAmount = (totalAmount * 10) + sender.tag
+            print(totalAmount)
+            print(sender.tag)
+            DispatchQueue.main.async {
+                self.amount.text = "EGP"
+                self.amount.text! += " \(self.totalAmount)"
+                
+            }
+        }
+        else{
+            AudioServicesPlaySystemSound(1521)
+        }
+        
+    }
+    
+    
+    @IBAction func deleteNumber(_ sender: UIButton) {
+        if(totalAmount == 0){
+            AudioServicesPlaySystemSound(1521)
+        }
+        else if totalAmount < 10{
+            totalAmount = 0
+            DispatchQueue.main.async {
+                self.amount.textColor = .systemGray5
+                self.amount.text = "EGP"
+                self.amount.text! += " \(self.totalAmount)"
+                
+            }
+        }
+        else{
+            totalAmount = totalAmount / 10
+            DispatchQueue.main.async {
+                self.amount.textColor = UIColor(named: "AccentColor")
+                self.amount.text = "EGP"
+                self.amount.text! += " \(self.totalAmount)"
+                
+            }
+        }
+        
+    }
+}
